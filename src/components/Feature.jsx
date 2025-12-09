6dfad0c03200abf7b4e51991c39f92e0eb1526b8
Mattgllrm <matthisgllrm@gmail.com>
2025-12-09 23:46:05 +0100

    Création appli en React

---
+++ src/components/Feature.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Feature({ icon, title, children }) {
+  return (
+    <div className="feature-item">
+      <img src={icon} alt={title} className="feature-icon" />
+      <h3 className="feature-item-title">{title}</h3>
+      <p>{children}</p>
+    </div>
+  );
+}
